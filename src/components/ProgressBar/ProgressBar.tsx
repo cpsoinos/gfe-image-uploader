3128ab386586155d2e4faf689c15594c62938cec
Corey Psoinos <coreypsoinos@gmail.com>
2024-07-18 13:38:01 -0400

    fix: use 'role' rather than 'aria-role'

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -8,7 +8,7 @@ export const ProgressBar: FC<ProgressBarProps> = ({ progress }) => {
   const progressId = useId()
 
   return (
-    <div className="flex items-center gap-4" aria-role="progressbar" aria-labelledby={progressId}>
+    <div className="flex items-center gap-4" role="progressbar" aria-labelledby={progressId}>
       <div className="relative h-1.5 w-full rounded-full bg-gray-200">
         <div
           className="duration-250 absolute left-0 top-0 h-full rounded-full bg-indigo-700 transition-[width] ease-linear"
