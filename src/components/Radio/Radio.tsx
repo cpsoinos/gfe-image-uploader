31673ac7374a79a1034659c63c46920eab55a489
Corey Psoinos <coreypsoinos@gmail.com>
2024-07-23 15:35:42 -0400

    fix: apply bg-cover and allow classes to be applied to radio

--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -1,9 +1,17 @@
 import { forwardRef, type InputHTMLAttributes } from 'react'
 import './Radio.styles.css'
+import { twMerge } from 'tailwind-merge'
 
 type RadioProps = InputHTMLAttributes<HTMLInputElement>
 
 export const Radio = forwardRef<HTMLInputElement, RadioProps>((props, ref) => {
-  return <input ref={ref} type="radio" {...props} className="focus:focus-band size-6 border-none" />
+  return (
+    <input
+      ref={ref}
+      type="radio"
+      {...props}
+      className={twMerge('focus:focus-band size-6 border-none bg-cover', props.className)}
+    />
+  )
 })
 Radio.displayName = 'Radio'
