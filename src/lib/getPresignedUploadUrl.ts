d7a494bca77c47ef5f444dc5fbac352d006e272d
Corey Psoinos <coreypsoinos@gmail.com>
2024-07-23 12:21:53 -0400

    chore: cf pages deployment (#5)

--- src/lib/getPresignedUploadUrl.ts
+++ src/lib/getPresignedUploadUrl.ts
@@ -1,11 +1,15 @@
 export const getPresignedUploadUrl = async (file: File) => {
   const key = file.name
   const contentType = file.type
-  const url = new URL(`${process.env.NEXT_PUBLIC_HOST}/api/images/${key}`)
+  const url = new URL(`/api/images/${key}`, location.origin)
   url.searchParams.append('contentType', contentType)
 
   const response = await fetch(url, { method: 'PUT' })
-  const signedUrl = await response.text()
 
-  return signedUrl
+  if (response.ok) {
+    const signedUrl = await response.text()
+    return signedUrl
+  } else {
+    throw new Error('Server error when attempting to generate presigned URL')
+  }
 }
