30dabc0a0bdafabbc0169ddb56162a2c7bda1807
Corey Psoinos <coreypsoinos@gmail.com>
2024-07-28 10:52:20 -0400

    fix: add custom not-found page so we can specify runtime = 'edge'

--- src/components/PostHog/PostHogIdentify.tsx
+++ src/components/PostHog/PostHogIdentify.tsx
@@ -6,15 +6,16 @@ import type { User } from '@/db/schema'
 
 export default function PostHogIdentify({ user }: { user?: User }): null {
   const posthog = usePostHog()
+  const { id, email } = user || {}
 
   useEffect(() => {
-    if (user) {
+    if (id && email && posthog) {
       // Identify sends an event, so you want may want to limit how often you call it
-      posthog?.identify(user.id, {
-        email: user.email,
+      posthog.identify(id, {
+        email: email,
       })
     }
-  }, [posthog, user])
+  }, [email, id, posthog])
 
   return null
 }
