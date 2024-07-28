984e45d2acb1570f5e9749d50167662d11661252
Corey Psoinos <coreypsoinos@gmail.com>
2024-07-28 11:01:23 -0400

    customize error view component and create error.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+'use client' // Error components must be Client Components
+
+import { useEffect } from 'react'
+import { ErrorView } from '@/components/ErrorView/ErrorView'
+
+export default function Error({ error }: { error: Error & { digest?: string } }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <ErrorView
+        title="Unexpected error"
+        description="We're facing some issues at the moment. Please try again later or contact support at"
+        link={{ href: '/', text: 'info@anderapps.com' }}
+      />
+    </div>
+  )
+}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,15 @@
-import { Link } from '@/components/Link/Link'
-import EmotionSadIcon from '@/icons/emotion-sad-line.svg'
+import { ErrorView } from '@/components/ErrorView/ErrorView'
 
 export const runtime = 'edge'
 
 export default function NotFound() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex flex-col items-center gap-5 p-6">
-        <EmotionSadIcon className="size-12 rounded-full bg-white p-3 text-indigo-700 shadow" />
-        <div className="flex flex-col items-center gap-2">
-          <h2 className="text-xl">Not found</h2>
-          <p>
-            Could not find requested resource.{' '}
-            <Link href="/" className="text-indigo-700">
-              Return home
-            </Link>
-          </p>
-        </div>
-      </div>
+      <ErrorView
+        title="Not found"
+        description="Could not find requested resource."
+        link={{ href: '/', text: 'Return home' }}
+      />
     </div>
   )
 }
