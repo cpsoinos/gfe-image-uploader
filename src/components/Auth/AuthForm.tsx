3800c2f479555a4d400eae6d6e72f5e3d8726b6a
Corey Psoinos <coreypsoinos@gmail.com>
2024-07-29 18:29:51 -0400

    fix: auth page styling

--- src/components/Auth/AuthForm.tsx
+++ src/components/Auth/AuthForm.tsx
@@ -6,7 +6,7 @@ import { Button } from '../Button/Button'
 
 export const AuthForm: FC = () => {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="flex min-h-full flex-1 flex-col justify-center gap-10 px-6 lg:px-8">
       <div className="flex flex-col gap-4 sm:mx-auto sm:w-full sm:max-w-md">
         <AnderAppsLogo className="mx-auto h-16 w-auto" />
         <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -15,8 +15,8 @@ export const AuthForm: FC = () => {
         <p className="text-center">So the images you upload and crop can be persisted</p>
       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+      <div className="sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="rounded-lg bg-white px-6 py-12 shadow sm:px-12">
           <div>
             <div className="flex flex-col gap-4">
               {Object.values(providerMap).map((provider) => (
